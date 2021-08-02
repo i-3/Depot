b3cb046b826151bf8c5987bf674d38d70c477938
i-3 <31x1962@gmail.com>
2021-08-02 21:52:40 +0300

    store_index_url -> store_url

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -60,7 +60,7 @@ class CartsController < ApplicationController
     @cart.destroy if @cart.id == session[:cart_id]
     session[:cart_id] = nil
     respond_to do |format|
-      format.html { redirect_to store_index_url }
+      format.html { redirect_to store_url }
       format.json { head :no_content }
     end
   end
@@ -78,6 +78,6 @@ class CartsController < ApplicationController
 
     def invalid_cart
       logger.error "Attempt to access invalid cart #{params[:id]}"
-      redirect_to store_index_url, notice: 'Invalid cart'
+      redirect_to store_url, notice: 'Invalid cart'
     end
 end
