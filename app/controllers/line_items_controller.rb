bb12ae10c894fddcfcd49ee7fd10003a769189d9
i-3 <31x1962@gmail.com>
2021-08-15 11:42:56 +0300

    Added Login button, user greeting & real time

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -72,6 +72,7 @@ class LineItemsController < ApplicationController
   end
 
   private
+  
   # Use callbacks to share common setup or constraints between actions.
   def set_line_item
     @line_item = LineItem.find(params[:id])
--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -79,6 +79,7 @@ class OrdersController < ApplicationController
   end
 
   private
+  
   # Use callbacks to share common setup or constraints between actions.
   def set_order
     @order = Order.find(params[:id])
